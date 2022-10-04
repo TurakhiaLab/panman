9da0a48e98b217ef6df89c2e3ac0c628c979cb3b
Harsh Motwani <harshmotwanimail@gmail.com>
2022-10-03 21:47:35 -0700

    Added files to test protobuff API by creating sample protobuf MAT tree and reading and printing it (read_sample.cpp and write_sample.cpp)

---
+++ PangenomeMAT.hpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <string>
+#include <fstream>
+
+namespace PangenomeMAT {
+
+	struct NucMut {
+		int32_t position;
+		int32_t gapPosition;
+		int32_t condensed; // The first two bits here should indicate the type of mutation and the other parameters would be read accordingly;
+		int32_t nucs;
+	};
+
+	struct BlockMut {
+		std::vector< int32_t > condensedBlockMuts;
+	};
+
+	struct BlockList {
+		std::vector< int32_t > blockIdsAndChromosomeNames; // Since blockId takes 3 bytes and chromosomeName takes one byte, we can store it efficiently
+	};
+
+	struct GapList {
+		std::vector< int32_t > position;
+		std::vector< int32_t > condensed;
+	};
+
+	class Node {
+	public:
+		Node* parent;
+		std::vector< Node* > children;
+		std::vector< NucMut > nucMutations;
+		BlockMut blockMutations;
+	};
+
+	class Tree {
+	private:
+		Node* getTreeFromNewickString(std::string newick);
+		std::unordered_map<std::string, Node*> nodes;
+		
+	public:
+		Tree(std::ifstream& fin);
+
+		Node* root;
+		GapList gaps;
+		BlockList blocks;
+	};
+
+};
