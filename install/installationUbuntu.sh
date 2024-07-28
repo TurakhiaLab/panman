0f2fcdf99f07e1b62fd31b8b030d41c39451dd09
sumit-walia <swalia@ucsd.edu>
2024-07-28 08:20:55 -0700

    updated workflow

--- install/installationUbuntu.sh
+++ install/installationUbuntu.sh
@@ -13,9 +13,6 @@ git clone https://github.com/microsoft/vcpkg.git
 wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
 tar -xvzf 2019_U9.tar.gz
 
-apt-get install protobuf-compiler libprotobuf-dev
-protoc --version
-which protoc
 
 cmake  -DTBB_DIR=${PWD}/oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DProtobuf_PROTOC_EXECUTABLE=/usr/bin/protoc -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 make -j
