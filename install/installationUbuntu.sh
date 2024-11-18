5e7628e50a22e306bea53be021fb73b4093d801c
sumit-walia <walia.sumit76@gmail.com>
2024-11-18 14:34:49 -0800

    solving workflow issue

--- install/installationUbuntu.sh
+++ install/installationUbuntu.sh
@@ -2,6 +2,8 @@
 
 sudo apt install -y git build-essential cmake wget curl zip unzip tar libboost-all-dev pkg-config capnproto
 
+which capnp
+
 # Build
 startDir=$pwd
 cd $(dirname "$0")
@@ -16,7 +18,7 @@ sudo apt-get install pkg-config
 wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
 tar -xvzf 2019_U9.tar.gz
 
-cmake  -DTBB_DIR=${PWD}/oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCapnProto_PATH=/usr/bin/capnp -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake  -DTBB_DIR=${PWD}/oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 
 make -j
 
