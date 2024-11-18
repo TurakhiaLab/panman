ffa7ba4506dad38c385aab97a626eb5b2c0d6599
sumit-walia <walia.sumit76@gmail.com>
2024-11-18 14:14:15 -0800

    updated scripts

--- install/installationUbuntu.sh
+++ install/installationUbuntu.sh
@@ -1,5 +1,5 @@
 # Install dependencies
-
+sudo apt install -y git build-essential cmake wget curl zip unzip tar libboost-all-dev pkg-config capnproto
 # Build
 startDir=$pwd
 cd $(dirname "$0")
@@ -15,7 +15,7 @@ wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz
 tar -xvzf 2019_U9.tar.gz
 
 
-cmake  -DTBB_DIR=${PWD}/oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DProtobuf_PROTOC_EXECUTABLE=/usr/bin/protoc -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake  -DTBB_DIR=${PWD}/oneTBB-2019_U9  -DCMAKE_PREFIX_PATH=${PWD}/oneTBB-2019_U9/cmake -DCapnProto_PATH=/usr/bin/capnp -DCMAKE_TOOLCHAIN_FILE=${PWD}/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 make -j
 
 cd $startDir
