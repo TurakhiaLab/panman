c01bfaddb2c675356602fd50b1eac144326d85cf
sumit-walia <walia.sumit76@gmail.com>
2024-11-18 14:49:43 -0800

    solving workflow issue

--- install/installationUbuntu.sh
+++ install/installationUbuntu.sh
@@ -1,14 +1,22 @@
 # Install dependencies
 
-sudo apt install -y git build-essential cmake wget curl zip unzip tar libboost-all-dev pkg-config capnproto
+sudo apt install -y git build-essential cmake wget curl zip unzip tar libboost-all-dev pkg-config
 
-which capnp
 
 # Build
 startDir=$pwd
 cd $(dirname "$0")
 mkdir -p ../build
 cd ../build
+
+# install capnp
+curl -O https://capnproto.org/capnproto-c++-1.0.2.tar.gz
+tar zxf capnproto-c++-1.0.2.tar.gz
+cd capnproto-c++-1.0.2
+./configure
+make -j6 check
+sudo make install
+which capnp
 
 git clone https://github.com/microsoft/vcpkg.git
 sudo apt-get install pkg-config
