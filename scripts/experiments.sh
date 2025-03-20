f99492b4cad17b1cf0fe28a0e7f929742bc8332c
sumit-walia <walia.sumit76@gmail.com>
2025-03-20 12:56:29 -0700

    updated zenodo link

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -64,7 +64,11 @@ python3 3seq2panman.py out.3s.rec out.3s.panman.rec info/out_0.msa
 T_OUT=twilight_out
 gdown --folder https://drive.google.com/drive/folders/10Vlycv5KBjwMB1RQfLR_CMBaqmniNpbf
 mkdir $T_OUT
-twilight -t SARS_8M/sars_8M.nwk -i <input sequences> -p y -v -r 1 --trans -8 -o $T_OUT/sars_8M_without_wuhan.msa --gap-open -60
+wget -O sars_8M.aln.gz.xz https://zenodo.org/records/15059329/files/sars_8M.aln.gz.xz?download=1
+xz -d sars_8M.aln.gz.xz
+gunzip sars_8M.aln.gz
+sed '/^>/!s/-//g' sars_8M.aln > sars_8M.fa
+twilight -t SARS_8M/sars_8M.nwk -i sars_8M.fa -p y -v -r 1 --trans -8 -o $T_OUT/sars_8M_without_wuhan.msa --gap-open -60
 cp SARS_8M/wuhan.fa $T_OUT
 twilight -f $T_OUT -a l -p y -v -r 1 --trans -8 -o sars_8M.msa --gap-open -60
 grep -a -A 1 Wuhan-Hu-1 sars_8M.msa > wuhan.aln
@@ -74,7 +78,7 @@ panmanUtils -I panman/sars_8M.panman --summary
 panmanUtils -I panman/sars_8M.panman --fasta
 panmanUtils -I panman/sars_8M.panman --vcf
 
-panmanUtils --create-network panman/sars_8M.panman --input-file SARS_8M/SARS_8M_cmut -o sars_8M_network
+panmanUtils --create-network panman/sars_8M.panman --input-file SARS_8M/sars_8M_cmut -o sars_8M_network
 panmanUtils -I panman/sars_8M_network.panman --summary
 
 
